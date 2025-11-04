c5bc79b65b12f1a7241e613a4bdc96250bd8d37e
Mathias Mariman <mathias.mariman@external.toyota-europe.com>
2025-11-04 12:43:26 +0100

    db reset

--- prisma/migrations/20251014084323_fish_rarity/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Fish" ADD COLUMN "rarity" TEXT;
--- prisma/migrations/20251104114233_initial/migration.sql
+++ prisma/migrations/20251104114233_initial/migration.sql
@@ -10,7 +10,8 @@ CREATE TABLE "DivingCenter" (
 CREATE TABLE "Fish" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "image" TEXT
+    "image" TEXT,
+    "rarity" TEXT
 );
 
 -- CreateTable
@@ -21,4 +22,20 @@ CREATE TABLE "FishSighting" (
     "longitude" REAL NOT NULL,
     "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "FishSighting_fishId_fkey" FOREIGN KEY ("fishId") REFERENCES "Fish" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "TemperatureSensor" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "latitude" REAL NOT NULL,
+    "longitude" REAL NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "TemperatureReading" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "temperature" DECIMAL NOT NULL,
+    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "temperatureSensorId" TEXT NOT NULL,
+    CONSTRAINT "TemperatureReading_temperatureSensorId_fkey" FOREIGN KEY ("temperatureSensorId") REFERENCES "TemperatureSensor" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
